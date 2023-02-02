4a55998b4c2012f1aaa479b11e9fc3e684c4262b
Angelique Joyce Garcia <angeliquejoyceg@gmail.com>
2023-02-03 06:25:34 +0800

    Fixing Bug in Dashboard

--- app/src/main/java/com/example/sd2thesis/Users.kt
+++ app/src/main/java/com/example/sd2thesis/Users.kt
@@ -1,12 +1,12 @@
 package com.example.sd2thesis
 
 data class Users( val answer : String ? = null,
-                  val birthDate : String ? = null,
-                  val email : String ? = null,
-                  val givenName : String ? = null,
-                  val gradeLevel : String ? = null,
+                  val bithday : String ? = null,
+                  val email_address : String ? = null,
+                  val first_name : String ? = null,
+                  val grade_level : String ? = null,
                   val password : String ? = null,
-                  val question: String ? = null,
+                  val security_question: String ? = null,
                   val school : String ? = null,
-                  val surname : String ? = null
+                  val last_name : String ? = null
                   )
