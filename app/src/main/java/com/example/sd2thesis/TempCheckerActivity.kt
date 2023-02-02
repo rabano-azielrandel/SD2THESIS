b44348c0c531b1e21a85bd52b0a8004f862fe619
Angelique Joyce Garcia <angeliquejoyceg@gmail.com>
2023-02-02 23:01:10 +0800

    Main and Spell Checker update
    adding new btn to main
    integrating nlp to system, but nltk is not yet installed

---
+++ app/src/main/java/com/example/sd2thesis/TempCheckerActivity.kt
@@ -0,0 +1,37 @@
+package com.example.sd2thesis
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//test
+import nltk.downloader.*
+import nltk.tokenize.*
+import nltk.corpus.*
+
+class TempCheckerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_temp_checker)
+
+        Downloader().download("punkt") // download the word tokenizer
+        Downloader().download("stopwords") // download the stopwords
+
+        val stopWords = stopwords.words("tagalog").toSet() // set of Tagalog stopwords
+
+        fun checkSpelling(text: String): List<String> {
+            // tokenize the words in the text
+            val words = word_tokenize(text)
+
+            // remove any stopwords and non-alphabetic characters
+            val filteredWords = words.filter { it.isAlpha() && it.toLowerCase() !in stopWords }
+
+            // spellcheck the remaining words using a spellchecker library (e.g. PySpellChecker)
+            // Add code here
+
+            return filteredWords
+        }
+
+        val text = "Ang mga tao ay nagtatrabaho sa opisina."
+        val correctedText = checkSpelling(text)
+        println(correctedText)
+    }
+}
