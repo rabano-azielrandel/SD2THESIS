60d32063f41083c632f802848bc64960b6e0ac1b
Angelique Joyce Garcia <angeliquejoyceg@gmail.com>
2023-03-07 11:04:17 +0800

    Saving a picture of the paintview when the button is clicked

--- app/src/main/java/com/example/sd2thesis/LetterSimulation.kt
+++ app/src/main/java/com/example/sd2thesis/LetterSimulation.kt
@@ -1,14 +1,16 @@
 package com.example.sd2thesis
 
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Path
+import android.graphics.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.widget.ImageButton
+import android.widget.Toast
 import com.example.sd2thesis.PaintView.Companion.colorList
 import com.example.sd2thesis.PaintView.Companion.currentBrush
 import com.example.sd2thesis.PaintView.Companion.pathList
+import java.io.File
+import java.io.FileOutputStream
 
 class LetterSimulation : AppCompatActivity() {
 
@@ -16,6 +18,8 @@ class LetterSimulation : AppCompatActivity() {
         var path = Path()
         var paintBrush = Paint()
     }
+
+    private lateinit var paintView: PaintView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,14 +32,27 @@ class LetterSimulation : AppCompatActivity() {
         val blackButton = findViewById<ImageButton>(R.id.blackColor);
         val clearFormat = findViewById<ImageButton>(R.id.whiteColor);
 
+        paintView = findViewById(R.id.paintView)
+
         redButton.setOnClickListener {
             paintBrush.setColor(Color.RED)
             currentColor(paintBrush.color)
         }
 
         blueButton.setOnClickListener {
-            paintBrush.setColor(Color.BLUE)
-            currentColor(paintBrush.color)
+            val bitmap = Bitmap.createBitmap(paintView.width, paintView.height, Bitmap.Config.ARGB_8888)
+            val canvas = Canvas(bitmap)
+            paintView.draw(canvas)
+
+            // Save the bitmap to the device's Downloads folder
+            val folder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+            val file = File(folder, "my_image.png")
+            val outputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+            outputStream.flush()
+            outputStream.close()
+
+            Toast.makeText(this, "Image saved to downloads", Toast.LENGTH_SHORT).show()
         }
 
         purpleButton.setOnClickListener {
